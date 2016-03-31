85aee7abd5aa59516f45b7a82ad27af4934a0248
Justin K Nam <justin.k.nam@gmail.com>
2016-03-31 18:11:38 +1100

    forgot what i did, forgot to push before i left library

---
+++ src/SearchBean.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+
+import java.io.Serializable;
+
+public class SearchBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private ArrayList <>
+}
